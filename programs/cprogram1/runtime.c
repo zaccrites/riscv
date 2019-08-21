60050a41fbfa0aed3b3e32a86bc810e2666d6010
Zac Crites <zac@criteshouse.com>
2019-08-21 09:25:45 -0400

    Update cprogram Makefile to use 32-bit libgcc

--- programs/cprogram1/runtime.c
+++ programs/cprogram1/runtime.c
@@ -40,24 +40,3 @@ void exit(void)
 {
     _exit();
 }
-
-
-
-
-
-
-// Until M extension is supported, I guess.
-// https://gcc.gnu.org/onlinedocs/gcc-3.1/gccint/Library-Calls.html
-int32_t __mulsi3(int32_t a, int32_t b)
-{
-    // This is probably extremely unoptimized.
-    int result = 0;
-    int counter = (b < 0) ? -b : b;
-    while (counter)
-    {
-        result += a;
-        counter -= 1;
-    }
-
-    return b < 0 ? -result : result;
-}
